8de6b0cc1d18d355dc1b408b5cbd42ee914b5b9f
Dino Puric <d.puric@sportradar.com>
2020-03-15 15:49:43 +0100

    html cleanup, css cleanup

--- app.js
+++ app.js
@@ -1,31 +1,20 @@
-const navSlide= () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLink = document.querySelector('.nav-links li')
-    //toggle nav
-    burger.addEventListener('click',() => {
-        nav.classList.toggle('nav-active');
-        
-    });
-    navLink.forEach((link, index) => {
-        nav.classList.toggle('nav-active');
-        
-        
-        navLink.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 2}s'
-                
-            }
-            
-            
-            });
-    });
-    //burger Animation
-    
-   
-    
-}
+ const burger = document.querySelector('.hamburger');
+ const nav = document.querySelector('.nav-links');
+ const navLink = document.querySelector('.nav-links li')
 
-navSlide();
+ //toggle nav
+ burger.addEventListener('click', () => {
+     nav.classList.toggle('nav-active');
+     burger.classList.toggle('is-active');
+ });
+ navLink.forEach((link, index) => {
+     nav.classList.toggle('nav-active');
+     navLink.forEach((link, index) => {
+         if (link.style.animation) {
+             link.style.animation = '';
+         } else {
+             link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 2}s'
+         }
+     });
+ });
+
